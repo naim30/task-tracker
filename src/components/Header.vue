919265e1579981a7713ad33b57274d017a525e3a
Naimish Lukhi <lukhinaimish@gmail.com>
2021-08-18 15:30:27 +0530

    create component

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["title"],
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+  margin-bottom: 20px;
+}
+</style>
